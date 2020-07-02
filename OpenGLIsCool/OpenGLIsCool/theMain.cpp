75b6760927e02f968c9cc7ebc1e521ca992fe89c
Michael Feeney <mfeeney@fanshawec.ca>
2020-07-02 17:26:21 -0400

    Added a basic shader manager.

--- OpenGLIsCool/OpenGLIsCool/theMain.cpp
+++ OpenGLIsCool/OpenGLIsCool/theMain.cpp
@@ -14,6 +14,8 @@
 
 #include <iostream>
 
+#include "cShaderManager.h"
+
 struct sVertex
 {
     float x, y, z;      // NEW! With Zs
@@ -31,25 +33,29 @@ sVertex vertices[6] =
 };
 
 
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"        
-"attribute vec3 vPos;\n"        
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
+// Yes, it's global. Just be calm, for now.
+cShaderManager* g_pShaderManager = 0;       // NULL
 
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
+
+//static const char* vertex_shader_text =
+//"#version 110\n"
+//"uniform mat4 MVP;\n"
+//"attribute vec3 vCol;\n"        
+//"attribute vec3 vPos;\n"        
+//"varying vec3 color;\n"
+//"void main()\n"
+//"{\n"
+//"    gl_Position = MVP * vec4(vPos, 1.0);\n"
+//"    color = vCol;\n"
+//"}\n";
+
+//static const char* fragment_shader_text =
+//"#version 110\n"
+//"varying vec3 color;\n"
+//"void main()\n"
+//"{\n"
+//"    gl_FragColor = vec4(color, 1.0);\n"
+//"}\n";
 
 static void error_callback(int error, const char* description)
 {
@@ -104,18 +110,40 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
+    //cShaderManager* g_pShaderManager = 0;
+    ::g_pShaderManager = new cShaderManager();          // HEAP  
 
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
+    cShaderManager::cShader vertShader;
+    cShaderManager::cShader fragShader;
 
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
+    vertShader.fileName = "simpleVertex.glsl";
+    fragShader.fileName = "simpleFragment.glsl";
+
+    if ( ! ::g_pShaderManager->createProgramFromFile( "SimpleShaderProg",
+                                                      vertShader, fragShader) )
+    {
+        // There was problem. 
+        std::cout << "ERROR: can't make shader program because: " << std::endl;
+        std::cout << ::g_pShaderManager->getLastError() << std::endl;
+        
+        // Exit program (maybe add some cleanup code)
+        return -1;
+    }//createProgramFromFile
+
+    //vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    //glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    //glCompileShader(vertex_shader);
+//
+    //fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    //glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    //glCompileShader(fragment_shader);
+//
+    //program = glCreateProgram();
+    //glAttachShader(program, vertex_shader);
+    //glAttachShader(program, fragment_shader);
+    //glLinkProgram(program);
+
+    program = ::g_pShaderManager->getIDFromFriendlyName("SimpleShaderProg");
 
     mvp_location = glGetUniformLocation(program, "MVP");
     vpos_location = glGetAttribLocation(program, "vPos");
@@ -181,6 +209,11 @@ int main(void)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+
+    delete ::g_pShaderManager;
+
+
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
