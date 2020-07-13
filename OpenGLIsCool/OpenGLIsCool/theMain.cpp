8c853e10cf8c8530d29f5060ba6e3e8d659eeb75
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-07-13 09:25:12 -0400

    end of checkpoint 4

--- OpenGLIsCool/OpenGLIsCool/theMain.cpp
+++ OpenGLIsCool/OpenGLIsCool/theMain.cpp
@@ -14,6 +14,7 @@ Checkpoint #03
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include <glm/gtc/type_ptr.hpp> // glm::value_ptr
 
+#include <sstream> 
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -36,7 +37,7 @@ std::vector<cMeshObject*> g_pVecMeshObjects;
 //glm::vec3 g_cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
 //glm::vec3 g_upVector = glm::vec3(0.0f, 1.0f, 0.0f);
 
-glm::vec3 g_cameraEye = glm::vec3(0.0f, 0.0f, 400.0f);
+glm::vec3 g_cameraEye = glm::vec3(0.0f, 0.0f, 3000.0f);
 glm::vec3 g_cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 g_upVector = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -66,16 +67,156 @@ static void error_callback(int error, const char* description)
 * W = backward
 * E = up
 * Q = down
+*
+* Z = scale selected object up
+* X = scale selected object down
+* F = jump forward (index++)
+* B = jump backward (index--)
+*
+* P = persist all cMeshObject in list to file.
+* L = load all cMeshObject from file "" to ::g_pVecMeshObjects
 */
+
+int selectedIndex = 0;
+
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	const float CAMERA_SPEED = 1.0f;
 
-	/*if (key == GLFW_KEY_L)
-		::g_pVecMeshObjects[0]->orientation.z = glm::radians((float)1.f);
+	if (key == GLFW_KEY_P)
+	{
+		std::ofstream storage;
+		storage.open("storage.txt", std::ios::trunc);
+		storage << ::g_pVecMeshObjects.size() << std::endl;
+		for (int i = 0; i < ::g_pVecMeshObjects.size(); i++)
+		{
+			storage << ::g_pVecMeshObjects.at(i)->meshName;
+			storage << std::endl;
+			// position
+			storage << ::g_pVecMeshObjects.at(i)->position.x
+				<< "," << ::g_pVecMeshObjects.at(i)->position.y
+				<< "," << ::g_pVecMeshObjects.at(i)->position.z
+				<< ",";
+			storage << std::endl;
+			// color
+			storage << ::g_pVecMeshObjects.at(i)->colorRGBA.x
+				<< "," << ::g_pVecMeshObjects.at(i)->colorRGBA.y
+				<< "," << ::g_pVecMeshObjects.at(i)->colorRGBA.z
+				<< "," << ::g_pVecMeshObjects.at(i)->colorRGBA.w
+				<< ",";
+			storage << std::endl;
+			// orientation
+			storage << ::g_pVecMeshObjects.at(i)->orientation.x
+				<< "," << ::g_pVecMeshObjects.at(i)->orientation.y
+				<< "," << ::g_pVecMeshObjects.at(i)->orientation.z
+				<< ",";
+			storage << std::endl;
+			// scale
+			storage << ::g_pVecMeshObjects.at(i)->scale << std::endl;
+			// isWireframe
+			storage << (::g_pVecMeshObjects.at(i)->isWireFrame) ? 1 : 0;
+			storage << std::endl;
+		}
+		storage << "#";
+		storage.close();
+	}
 
-	if (key == GLFW_KEY_K)
-		::g_pVecMeshObjects[0]->orientation.z -= glm::radians((float)1.f);*/
+	if (key == GLFW_KEY_L && action == GLFW_PRESS)
+	{
+		::g_pVecMeshObjects.clear();
+		std::ifstream storage;
+		storage.open("storage.txt");
+
+		std::string line;
+		std::string token;
+
+		std::string delimeter = ",";
+		size_t pos = 0;
+
+		storage >> line;
+		size_t size = 0;
+		std::stringstream(line) >> size;
+
+		int counter = 0;
+
+		for (size_t i = 0; i < size; i++)
+		{
+			storage >> line;
+			if (line.find("assets/models/") != std::string::npos)
+			{
+				cMeshObject* pMesh = new cMeshObject();
+				// read mesh name
+				pMesh->meshName = line;
+				// read position
+				storage >> line;
+				while ((pos = line.find(delimeter)) != std::string::npos)
+				{
+					token = line.substr(0, pos);
+					line.erase(0, pos + delimeter.length());
+					counter++;
+					if (counter == 1) pMesh->position.x = std::stof(token);
+					else if (counter == 2) pMesh->position.y = std::stof(token);
+					else pMesh->position.z = std::stof(token);
+				}
+				counter = 0; // reset
+				// read rgba
+				storage >> line;
+				while ((pos = line.find(delimeter)) != std::string::npos)
+				{
+					token = line.substr(0, pos);
+					line.erase(0, pos + delimeter.length());
+					counter++;
+					if (counter == 1) pMesh->colorRGBA.x = std::stof(token);
+					else if (counter == 2) pMesh->colorRGBA.y = std::stof(token);
+					else if (counter == 3) pMesh->colorRGBA.z = std::stof(token);
+					else pMesh->colorRGBA.w = std::stof(token);
+				}
+				counter = 0; // reset
+				// read orientation
+				storage >> line;
+				while ((pos = line.find(delimeter)) != std::string::npos)
+				{
+					token = line.substr(0, pos);
+					line.erase(0, pos + delimeter.length());
+					counter++;
+					if (counter == 1) pMesh->orientation.x = std::stof(token);
+					else if (counter == 2) pMesh->orientation.y = std::stof(token);
+					else pMesh->orientation.z = std::stof(token);
+				}
+				counter = 0; // reset
+				// read scale
+				storage >> line;
+				pMesh->scale = std::stof(line);
+				// read isWireFrame
+				storage >> line;
+				pMesh->isWireFrame = (std::stof(line) == 1.0) ? true : false;
+				::g_pVecMeshObjects.push_back(pMesh);
+			}
+		} // end for
+		storage.close();		
+	}
+
+	if (key == GLFW_KEY_F && action == GLFW_PRESS)
+	{
+		if (selectedIndex == g_pVecMeshObjects.size() - 1) return;
+		selectedIndex++;
+	}
+
+	if (key == GLFW_KEY_B && action == GLFW_PRESS)
+	{
+		if (selectedIndex == 0) return;
+		selectedIndex--;
+	}
+
+	if (key == GLFW_KEY_Z)
+	{
+		g_pVecMeshObjects[selectedIndex]->scale *= 2.f;
+	}
+
+	if (key == GLFW_KEY_X)
+	{
+		g_pVecMeshObjects[selectedIndex]->scale *= 0.5f;
+	}
 
 	if (key == GLFW_KEY_A)
 		::g_cameraEye.x -= CAMERA_SPEED;
@@ -188,22 +329,83 @@ int main(void)
 		std::cout << "Error: " << ::g_pVAOManager->getLastError() << std::endl;
 	}
 
+	/*sModelDrawInfo mdiGourard;
+	if (!::g_pVAOManager->LoadModelIntoVAO("assets/models/gourard_xyz_rgba.ply", mdiGourard, program))
+	{
+		std::cout << "Error: " << ::g_pVAOManager->getLastError() << std::endl;
+	}
+
+	sModelDrawInfo mdiCorellianCorvette;
+	if (!::g_pVAOManager->LoadModelIntoVAO("assets/models/Corellian_Corvette_(Blockade_Runner)_xyz_rgba.ply", mdiCorellianCorvette, program))
+	{
+		std::cout << "Error: " << ::g_pVAOManager->getLastError() << std::endl;
+	}
+
+	sModelDrawInfo mdiSeaFloor;
+	if (!::g_pVAOManager->LoadModelIntoVAO("assets/models/Seafloor2_xyz_rgba.ply", mdiSeaFloor, program))
+	{
+		std::cout << "Error: " << ::g_pVAOManager->getLastError() << std::endl;
+	}
+
+	sModelDrawInfo mdiSu74;
+	if (!::g_pVAOManager->LoadModelIntoVAO("assets/models/su47_xyz_rgba.ply", mdiSu74, program))
+	{
+		std::cout << "Error: " << ::g_pVAOManager->getLastError() << std::endl;
+	}
+
+	sModelDrawInfo mdiPacificCod0;
+	if (!::g_pVAOManager->LoadModelIntoVAO("assets/models/PacificCod0_xyz_rgba.ply", mdiPacificCod0, program))
+	{
+		std::cout << "Error: " << ::g_pVAOManager->getLastError() << std::endl;
+	}*/
+
 	/**
 	Add to the list of cMeshObjects to draw.
 	*/
 
 	cMeshObject* pDolphin = new cMeshObject();
 	pDolphin->meshName = "assets/models/dolphin_color.ply";
-	pDolphin->position.x = -20.0f;
-	pDolphin->scale = 0.5f; // half of its original size
+	pDolphin->scale = 0.7f; // half of its original size
 	::g_pVecMeshObjects.push_back(pDolphin);
 
-	cMeshObject* pDolphin2 = new cMeshObject();
-	pDolphin2->meshName = "assets/models/dolphin_color.ply";
-	pDolphin2->position.x = 20.0f;
-	pDolphin2->scale = 0.5f; // half of its original size
-	pDolphin2->orientation.z = glm::radians(135.0f); // rotate around the z-axis 135 degree
-	::g_pVecMeshObjects.push_back(pDolphin2);
+	cMeshObject* pBunny = new cMeshObject();
+	pBunny->meshName = "assets/models/bun_zipper_res4.ply";
+	pBunny->scale = 1000.f;
+	pBunny->position.y += 200.0f;
+	::g_pVecMeshObjects.push_back(pBunny);
+
+	//cMeshObject* pGourard = new cMeshObject();
+	//pGourard->meshName = "assets/models/gourard_xyz_rgba.ply";
+	//pGourard->position.x -= 700.0f;
+	//pGourard->position.y -= 1000.0f;
+	//pGourard->scale = 0.10f; // half of its original size
+	////pGourard->orientation.z = glm::radians(135.0f); // rotate around the z-axis 135 degree
+	//::g_pVecMeshObjects.push_back(pGourard);
+
+	//cMeshObject* pCorellianCorvette = new cMeshObject();
+	//pCorellianCorvette->meshName = "assets/models/Corellian_Corvette_(Blockade_Runner)_xyz_rgba.ply";
+	//pCorellianCorvette->scale = 10.f;
+	//pCorellianCorvette->position.y -= 250.0f;
+	//::g_pVecMeshObjects.push_back(pCorellianCorvette);
+
+	//cMeshObject* pSeaFloor = new cMeshObject();
+	//pSeaFloor->meshName = "assets/models/Seafloor2_xyz_rgba.ply";
+	//pSeaFloor->scale = 5.f;
+	//pSeaFloor->position.y -= 500.0f;
+	//::g_pVecMeshObjects.push_back(pSeaFloor);
+
+	//cMeshObject* pSu74 = new cMeshObject();
+	//pSu74->meshName = "assets/models/su47_xyz_rgba.ply";
+	//pSu74->scale = 1500.f;
+	//pSu74->position.y += 500.0f;
+	//::g_pVecMeshObjects.push_back(pSu74);
+
+	//cMeshObject* pPacificCod0 = new cMeshObject();
+	//pPacificCod0->meshName = "assets/models/PacificCod0_xyz_rgba.ply";
+	//pPacificCod0->scale = 1700.f;
+	//pPacificCod0->position.y += 500.0f;
+	//pPacificCod0->position.x -= 700.0f;
+	//::g_pVecMeshObjects.push_back(pPacificCod0);
 
 	while (!glfwWindowShouldClose(window)) {
 		float ratio;
@@ -227,7 +429,7 @@ int main(void)
 		p = glm::perspective(0.6f,
 			ratio,
 			0.1f,			// near plane, anything nearer than this won't be drawn.
-			100000.0f); // far plane, anything further than this won't be drawn.
+			50000.0f); // far plane, anything further than this won't be drawn.
 
 		v = glm::mat4(1.0f);
 
